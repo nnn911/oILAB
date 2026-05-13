5c9f3c58edb0ac20987453e2bed69f98b2d3b41a
Daniel Utt <utt@ovito.org>
2026-05-13 14:48:33 +0200

    Fix examples

--- include/Lattices/ReciprocalLatticeDirectionImplementation.h
+++ include/Lattices/ReciprocalLatticeDirectionImplementation.h
@@ -9,6 +9,7 @@
 #ifndef OILAB_RECIPROCAL_LATTICE_DIRECTION_IMPL_H
 #define OILAB_RECIPROCAL_LATTICE_DIRECTION_IMPL_H
 
+#include "../Math/RLLL.h"
 #include "ReciprocalLatticeDirection.h"
 #include <ostream>
 
